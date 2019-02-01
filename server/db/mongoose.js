5b8c52cb37301f3cb35bb9ea2463cb23c8436280
udgin80 <udgin.80@mail.ru>
2019-02-01 23:15:38 +0300

    Setup env

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,9 +2,12 @@ var mongoose = require('mongoose');
 
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser: true});
+mongoose.connect( process.env.MONGODB_URI, {useNewUrlParser: true});
 
 
 module.exports = {
     mongoose
 }
+
+
+process.env.NODE_ENV
--- server/server.js
+++ server/server.js
@@ -1,3 +1,17 @@
+var env = process.env.NODE_ENV || 'development';
+console.log('env ******', env);
+
+if(env === 'development'){
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+} else if(env === 'test'){
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+
+}
+
+
+
+
+
 var {mongoose} = require('./db/mongoose');
 var express = require('express');
 var bodyParser = require('body-parser');
